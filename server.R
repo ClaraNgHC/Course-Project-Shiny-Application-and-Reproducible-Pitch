234ef6b49d5a3356130b6b9eb493ffe2a2afe8d1
ClaraNgHC <violetseablue@gmail.comgit config -- global user.name ClaraNgHC>
2017-05-10 15:41:10 +0800

    Revised

--- server.R
+++ server.R
@@ -11,9 +11,8 @@ library(shiny)
 
 # To do a simple calculations
 shinyServer(function(input, output) {
-   
   
-  output$res1<- renderPrint({
+  output$res1<- renderText({
     # USD exchange Rate base on the country selected dated as 22/4/2017
     USDMYR <- 4.40
     USDSGD <- 1.40
@@ -24,7 +23,7 @@ shinyServer(function(input, output) {
     USDAmt <- input$num
     
     #Doing simple calculations
-    if (input$Currency == 'RM') 
+    if (input$Currency == 'MYR') 
       Result1 <- USDAmt * USDMYR
     if (input$Currency == 'SG')
       Result1 <- USDAmt * USDSGD
@@ -32,30 +31,100 @@ shinyServer(function(input, output) {
       Result1 <- USDAmt * USDBAHT
     if (input$Currency == 'AUS')
       Result1 <- USDAmt * USDAUD
- 
-    list(Converted = Result1)
-  
-  })
-  
-  output$Select <- renderPrint({
-    if (input$Currency == 'RM') 
-      Result2 <- "Malaysia Ringgit (MYR)"
-    if (input$Currency == 'SG')
-      Result2 <- "Singapore Dollar (SGD)"
-    if (input$Currency == 'Thai')
-      Result2 <- "Thai Baht (BAHT)"
-    if (input$Currency == 'AUS')
-      Result2 <- "Australian Dollar (AUD)"
+    if (input$Currency == '0')
+      Result1 <- 0
     
-    #print country 
-    list(Country = Result2)
+    paste("USD", USDAmt , " = ", Result1 ,input$Currency )
   })
   
-  output$USD1 <- renderPrint({
+  output$Select <- renderText({
+    #Applying the full name for the currency
+    if (input$Currency == 'MYR') 
+      country2 <- "MYR = Malaysia Ringgit"
+    if (input$Currency == 'SG')
+      country2 <- "SGD = Singapore Dollar"
+    if (input$Currency == 'Thai')
+      country2 <- "BAHT = Thai Baht"
+    if (input$Currency == 'AUS')
+      country2 <- "AUD = Australian Dollar"
+    if (input$Currency == '0')
+      country2 <- "Null"
+    
+    paste(country2)
+  })
+  
+  output$USD1 <- renderText({
+    
+    USDMYR <- 4.40
+    USDSGD <- 1.40
+    USDBAHT <- 34.36
+    USDAUD <- 1.33
+    
+    # Get value on the amount want to convent
+    USDAmt <- input$num
+    
+    #Picking the right amount
+    if (input$Currency == 'MYR') 
+      Result3 <- USDMYR
+    if (input$Currency == 'SG')
+      Result3 <- USDSGD
+    if (input$Currency == 'Thai')
+      Result3 <- USDBAHT
+    if (input$Currency == 'AUS')
+      Result3 <- USDAUD
+    if (input$Currency == '0')
+      Result3 <- 0
+    
+    #point back the country currency name
+    if (input$Currency == 'MYR') 
+      Countsign <- "MYR"
+    if (input$Currency == 'SG')
+      Countsign <- "SGD"
+    if (input$Currency == 'Thai')
+      Countsign <- "BAHT"
+    if (input$Currency == 'AUS')
+      Countsign <- "AUD"
+    if (input$Currency == '0')
+      Countsign <- 0
+    
+    paste("1 USD =  ", Result3 ,Countsign)
+    
+  })
+  
+  output$ResInfo <- renderText({
+    USDMYR <- 4.40
+    USDSGD <- 1.40
+    USDBAHT <- 34.36
+    USDAUD <- 1.33
+    
     # Get value
     USDAmt <- input$num
     
-    list(USD = USDAmt)
+    #Doing simple calculations
+    if (input$Currency == 'MYR') 
+      Result2 <- 1 / USDMYR
+    if (input$Currency == 'SG')
+      Result2 <- 1 / USDSGD
+    Result1 <- USDAmt * USDSGD
+    if (input$Currency == 'Thai')
+      Result2 <- 1 / USDBAHT
+    if (input$Currency == 'AUS')
+      Result2 <- 1 / USDAUD
+    if (input$Currency == '0')
+      Result2 <- 0
+    
+    #point back the country currency name
+    if (input$Currency == 'MYR') 
+      Countsign <- "MYR"
+    if (input$Currency == 'SG')
+      Countsign <- "SGD"
+    if (input$Currency == 'Thai')
+      Countsign <- "BAHT"
+    if (input$Currency == 'AUS')
+      Countsign <- "AUD"
+    if (input$Currency == '0')
+      Countsign <- 0
+    
+    paste("1",Countsign , "= ", Result2 , "USD" )
   })
-  
-})
+}) 
--- ui.R
+++ ui.R
@@ -18,28 +18,46 @@ shinyUI(fluidPage(
   # Sidebar to select the country and also the amount wanted to convert
   sidebarLayout(
     sidebarPanel(
-        h3("USD excharge rate as at 22/4/2017"),
-    
-        selectInput("Currency", "Currency:",
-                   c("Malaysia Ringgit" = "RM",
-                     "Singapore Dollar" = "SG",
-                     "Thai Baht" = "Thai",
-                     "Australian Dollar" = "AUS",choices = NULL)),
-        numericInput("num", "How much USD you would like to convert?",
-                    value = 0,min = 1),
- 
-        submitButton("Submit")
+      h3("USD excharge rate as at 22/4/2017"),
+      
+      selectInput("Currency", "Currency:",
+                  c("Please select" = "0",
+                    "Malaysia Ringgit" = "MYR",
+                    "Singapore Dollar" = "SG",
+                    "Thai Baht" = "Thai",
+                    "Australian Dollar" = "AUS")),
+      numericInput("num", "How much USD you would like to convert?",
+                   value = 0,min = 1),
+      
+      submitButton("Submit")
     ),
     
     
-                
+    
     # Show the result of the converted amount
     mainPanel(
-       helpText('USD = United States Dollar'),
-       verbatimTextOutput('USD1'),
-       helpText('TO'),
-       verbatimTextOutput('Select'),
-       verbatimTextOutput('res1')
+      img(src="USflag.jpg", height = 120, width = 120),
+      img(src="msiaflag.jpg", height = 100, width = 100),
+      img(src="SGflag.jpg", height = 100, width = 100),
+      img(src="Thaiflag.jpg", height = 100, width = 125),
+      
+      h1(textOutput('res1')),
+      br(),
+      helpText(  
+        h3("Breakdown")),
+      h3(textOutput('USD1')),
+      h3(textOutput('ResInfo')),
+      
+      br(),
+      helpText(  
+        h3("Info:")),
+      helpText( 
+        h4("USD = United States Dollar ")),
+      h4(textOutput('Select'))
+      
+      
+      
+      
     )
   )
 ))
